0137976f2a0baf521c2699c60bbb1d5d1d6efc99
jdhilburn04 <dylan.hilburn@bruins.belmont.edu>
2025-06-13 15:24:43 -0500

    created game states

--- raylib_tutorial/main.cpp
+++ raylib_tutorial/main.cpp
@@ -1,24 +1,28 @@
 #include "raylib.h"
 #include <string>
-#define NUM_FRAME 3
+
+enum GameScreen { TITLE, ONE_PLAYER, TWO_PLAYER };
 
 int main() {
-    // window variables
+    // Window setup
     int screen_width = 800;
     int screen_height = 600;
+    InitWindow(screen_width, screen_height, "PONG");
+
+    // Game state variable
+    GameScreen currentScreen = TITLE;
+
+    // Title screen text
     const char* title_text = "PONG";
-
-    InitWindow(screen_width, screen_height, title_text);
-
-    // title screen title_text variables
     const char* one_player_button_text = "ONE PLAYER";
     const char* two_player_button_text = "TWO PLAYER";
-    int titleButtonTextSize = 25;
-    int title_text_posX = (static_cast<int>(screen_width) / 2) - 75;
-    //int title_text_posY = static_cast<int>(screen_height) / 2;
-    int title_text_posY = 100;
-    Vector2 onePlayerTextSize = MeasureTextEx(GetFontDefault(), one_player_button_text, titleButtonTextSize, 1);
-    Vector2 twoPlayerTextSize = MeasureTextEx(GetFontDefault(), two_player_button_text, titleButtonTextSize, 1);
+    int titleFontSize = 50;
+    int buttonFontSize = 25;
+
+    // Measure button sizes
+    Vector2 onePlayerTextSize = MeasureTextEx(GetFontDefault(), one_player_button_text, buttonFontSize, 1);
+    Vector2 twoPlayerTextSize = MeasureTextEx(GetFontDefault(), two_player_button_text, buttonFontSize, 1);
+
     Rectangle onePlayerButton = {
         screen_width / 2 - onePlayerTextSize.x / 2 - 10,
         screen_height / 2 - onePlayerTextSize.y / 2 + 50,
@@ -32,44 +36,47 @@ int main() {
         twoPlayerTextSize.y + 20
     };
 
-    bool clicked = false;
-
     SetTargetFPS(60);
 
-    // main game loop
-    
     while (!WindowShouldClose()) {
-        Vector2 mouse = GetMousePosition();
-        bool hoveringOne = CheckCollisionPointRec(mouse, onePlayerButton);
-        bool hoveringTwo = CheckCollisionPointRec(mouse, twoPlayerButton);
-        bool pressedOne = hoveringOne && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
-        bool pressedTwo = hoveringTwo && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
-
-        if (pressedOne || pressedTwo) clicked = true;
-
-        // screen background
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // button backgroundd
-        DrawRectangleRec(onePlayerButton, hoveringOne ? LIGHTGRAY : GRAY);
-        DrawRectangleRec(twoPlayerButton, hoveringTwo ? LIGHTGRAY : GRAY);
+        if (currentScreen == TITLE) {
+            Vector2 mouse = GetMousePosition();
+            bool hoveringOne = CheckCollisionPointRec(mouse, onePlayerButton);
+            bool hoveringTwo = CheckCollisionPointRec(mouse, twoPlayerButton);
 
-        // button border
-        DrawRectangleLinesEx(onePlayerButton, 2, WHITE);
-        DrawRectangleLinesEx(twoPlayerButton, 2, WHITE);
+            if (hoveringOne && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                currentScreen = ONE_PLAYER;
+            }
+            else if (hoveringTwo && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                currentScreen = TWO_PLAYER;
+            }
 
-        // title screen text
-        DrawText(title_text, title_text_posX, title_text_posY, 50, WHITE);
-        DrawText(one_player_button_text, onePlayerButton.x + 5, onePlayerButton.y + 10, titleButtonTextSize, WHITE);
-        DrawText(two_player_button_text, twoPlayerTextSize.x + 160, twoPlayerButton.y + 10, titleButtonTextSize, WHITE);
+            // Draw title
+            int titleWidth = MeasureText(title_text, titleFontSize);
+            DrawText(title_text, screen_width / 2 - titleWidth / 2, 100, titleFontSize, WHITE);
 
-        if (clicked) {
-            DrawText("Button clicked!", 10, 10, 20, RED);
+            // Draw buttons
+            DrawRectangleRec(onePlayerButton, hoveringOne ? LIGHTGRAY : GRAY);
+            DrawRectangleLinesEx(onePlayerButton, 2, WHITE);
+            DrawText(one_player_button_text, onePlayerButton.x + 10, onePlayerButton.y + 10, buttonFontSize, WHITE);
+
+            DrawRectangleRec(twoPlayerButton, hoveringTwo ? LIGHTGRAY : GRAY);
+            DrawRectangleLinesEx(twoPlayerButton, 2, WHITE);
+            DrawText(two_player_button_text, twoPlayerButton.x + 10, twoPlayerButton.y + 10, buttonFontSize, WHITE);
+        }
+        else if (currentScreen == ONE_PLAYER) {
+            DrawText("ONE PLAYER MODE", 250, 280, 30, GREEN);
+        }
+        else if (currentScreen == TWO_PLAYER) {
+            DrawText("TWO PLAYER MODE", 250, 280, 30, ORANGE);
         }
 
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
